07a2b2f32aae03008a7d653fbf9994e429d558c9
Emily Sheehan <69002400+emilyws27@users.noreply.github.com>
2021-11-05 11:53:36 -0500

    Create pieces.cpp

---
+++ pieces.cpp
@@ -0,0 +1,6 @@
+#include "pieces.h"
+
+
+game_pieces::game_pieces() : name(" "), appearance(" ") {}
+game_pieces::game_pieces(std::string name, std::string appearance) :name(name), appearance(appearance) {
+}
